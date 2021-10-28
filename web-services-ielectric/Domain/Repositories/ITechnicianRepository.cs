4397cd38bf0e00527f3057b33a100b26bbd55bbc
SebastianBran <sebastianbranzapata@gmail.com>
2021-10-28 14:52:10 -0500

    feat: technicians domain layer
    I created the technician model, the interfaces for the service and the repository, and the class for communication.

---
+++ web-services-ielectric/Domain/Repositories/ITechnicianRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_services_ielectric.Domain.Models;
+
+namespace web_services_ielectric.Domain.Repositories
+{
+    public interface ITechnicianRepository
+    {
+        Task<IEnumerable<Technician>> ListAsync();
+        Task AddAsync(Technician technician);
+        Task<Technician> FindByIdAsync(long id);
+        void Update(Technician technician);
+        void Remove(Technician technician);
+    }
+}
---
+++ web-services-ielectric/Domain/Services/Communication/TechnicianResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_services_ielectric.Domain.Services.Communication
+{
+    public class TechnicianResponse
+    {
+    }
+}
