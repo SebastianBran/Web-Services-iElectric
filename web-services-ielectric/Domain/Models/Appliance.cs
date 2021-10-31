ead2f629c74d0661abb30fe2cb8695f6c8d93631
Sebastian <s.gamarra9977@hotmail.com>
2021-10-31 14:42:03 -0500

    feat: Appliances domain layer

---
+++ web-services-ielectric/Domain/Models/Appliance.cs
@@ -0,0 +1,12 @@
+﻿
+namespace web_services_ielectric.Domain.Models
+{
+    public class Appliance
+    {
+        //Properties
+        public long Id { get; set; }
+        public long ApplianceModelId { get; set; }
+        public long ClientId { get; set; }
+        public int PurchaseDate { get; set; }
+    }
+}
---
+++ web-services-ielectric/Domain/Services/Communication/ApplianceResponse.cs
@@ -0,0 +1,10 @@
+﻿using web_services_ielectric.Domain.Models;
+
+namespace web_services_ielectric.Domain.Services.Communication
+{
+    public class ApplianceResponse:BaseResponse<Appliance>
+    {
+        public ApplianceResponse(string message) : base(message) { }
+        public ApplianceResponse(Appliance appliance) : base(appliance) { }
+    }
+}
