1cf880342bdab000a801cff3cbfc2f3be0c5e1b2
Sebastian <s.gamarra9977@hotmail.com>
2021-11-04 16:05:38 -0500

    feat: ApplianceBrand resource and service

---
+++ web-services-ielectric/Resources/ApplianceBrandResource.cs
@@ -0,0 +1,9 @@
+﻿namespace web_services_ielectric.Resources
+{
+    public class ApplianceBrandResource
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string ImgPath { get; set; }
+    }
+}
---
+++ web-services-ielectric/Resources/SaveApplianceBrandResource.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace web_services_ielectric.Resources
+{
+    public class SaveApplianceBrandResource
+    {
+        [Required]
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string ImgPath { get; set; }
+    }
+}
