66e5af89f9fa3457483bf8de1e332d0a356f7b15
carlos130702 <midoriya3612@gmail.com>
2021-11-11 19:12:10 -0500

    feat: administrators domain layer
    I created the administrator model, the interfaces for the service and the repository, and the class for communication.

---
+++ web-services-ielectric/Administrators/Domain/Repositories/IAdministratorRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_services_ielectric.Domain.Models;
+
+namespace web_services_ielectric.Domain.Repositories
+{
+    public interface IAdministratorRepository
+    {
+        Task<IEnumerable<Administrator>> ListAsync();
+        Task AddAsync(Administrator administrator);
+        Task<Administrator> FindByIdAsync(long id);
+        void Update(Administrator administrator);
+        void Remove(Administrator administrator);
+    }
+}
---
+++ web-services-ielectric/Administrators/Domain/Services/Communication/AdministratorResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_services_ielectric.Domain.Models;
+
+namespace web_services_ielectric.Domain.Services.Communication
+{
+    public class AdministratorResponse : BaseResponse<Administrator>
+    {
+        public AdministratorResponse(string message) : base(message) { }
+        public AdministratorResponse(Administrator resource) : base(resource) { }
+    }
+}
